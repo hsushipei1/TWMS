5f643bbc5e72f9649224658dcf27f027b13438b0
hsushipei <hsushipei1@gmail.com>
2019-07-03 12:27:51 +0800

    1. Delete unecessary code (merge into one daily data file in "merge_sliced.py") 2. Add function for FNL daily data compression.

--- download_data/merge_sliced/merge_sliced.py
+++ download_data/merge_sliced/merge_sliced.py
@@ -2,6 +2,7 @@
 import os, pickle, datetime, sys
 #CDO = "~/software/PREVIOUS_SOFTWARE/cdo-1.9.3/bin/cdo -O "
 CDO = "~/software/bin/cdo -O "
+NCCOPY = "~/software/bin/nccopy"
 slic_6h_dir = "/data5/hsushipei/data/fnl/sliced/latest/6hr/"
 slic_day_dir = "/data5/hsushipei/data/fnl/sliced/latest/daily/"
 
@@ -19,7 +20,7 @@ def sixhr2daily( indv_6h_files, singldaily_file, varname_ori, varname_chd ):
 	Merge 6 hr data to daily data and compress it. 
 	"""
 	#os.system(CDO+" -a -z zip_2 -f nc4 -shifttime,-9hours -daymean -chname,'"+varname_ori+"','"+varname_chd+"' -cat '"+indv_6h_files+"' "+singldaily_file)
-	os.system(CDO+" -a -f nc -shifttime,-9hours -daymean -chname,'"+varname_ori+"','"+varname_chd+"' -cat '"+indv_6h_files+"' "+singldaily_file)
+	os.system(CDO+" -a -f nc -shifttime,-9hours -daymean -chname,'"+varname_ori+"','"+varname_chd+"' -cat '"+indv_6h_files+"' "+singldaily_file) # without compression
 
 def merg2_long_day_data(long_term_daily_file, singldaily_file ):
 	"""
@@ -28,11 +29,16 @@ def merg2_long_day_data(long_term_daily_file, singldaily_file ):
 	os.system(CDO+" mergetime "+long_term_daily_file+" "+singldaily_file+" "+long_term_daily_file+".temp")
 	os.system("mv "+long_term_daily_file+".temp "+long_term_daily_file)
 
-def to_rel_time(input_file, output_file):
+def compress_daily(infile, outfile):
 	"""
-	Create relative time coordinate version for grads
+	Compress each daily data with level=2 (compression rate is about 33% and spent around 6 sec.)
+
+	Arguments:
+	infile: input file (uncompressed file)
+	outfile: output file (compressed file) 
 	"""
-	os.system(CDO+" -r copy "+input_file+" "+output_file)
+	ncco_lev = 2
+	os.system(NCCOPY+" -d "+ncco_lev+" "+infile+" "+outfile)
 
 
 # At 18Z, run the following procedure
@@ -45,25 +51,7 @@ if crtH == "18":
 	sixhr2daily( slic_6h_dir+"fnl025_v_"+crtyyyymmdd+"*.nc4", slic_day_dir+"fnl025_v_"+crtyyyymmdd+".nc4","VGRD","v")
 	sixhr2daily( slic_6h_dir+"fnl025_hgt_"+crtyyyymmdd+"*.nc4", slic_day_dir+"fnl025_hgt_"+crtyyyymmdd+".nc4","HGT","HGT")
 
-	# Merge daily data with long daily data and save file to "slic_day_dir"
-	"""
-	Stop this procedure to save disk usage since data in multiple time steps can be read by "addfiles" in NCL
-	20180413
-
-	merg2_long_day_data( slic_day_dir+"fnl025_u_201601_latest_daily.nc",\
-			slic_day_dir+"fnl025_u_"+crtyyyymmdd+".nc" ) # u
-	merg2_long_day_data( slic_day_dir+"fnl025_v_201601_latest_daily.nc",\
-			slic_day_dir+"fnl025_v_"+crtyyyymmdd+".nc" ) # v
-	merg2_long_day_data( slic_day_dir+"fnl025_pw_201601_latest_daily.nc",\
-			slic_day_dir+"fnl025_pw_"+crtyyyymmdd+".nc" ) # v
-
-	if int(crtd)%2 == 0: # on even day, create relative time coordinate version for grads
-		print "YES"
-		to_rel_time( slic_day_dir+"fnl025_u_201601_latest_daily.nc",\
-				slic_day_dir+"fnl025_u_201601_latest_daily_grads.nc") # u
-		to_rel_time( slic_day_dir+"fnl025_v_201601_latest_daily.nc",\
-				slic_day_dir+"fnl025_v_201601_latest_daily_grads.nc") # v
-	"""
+	# Compress (with nccopy) daily data 
 
 else:
 	print "t= "+crtH+" Wait untill 18Z to create daily data."
