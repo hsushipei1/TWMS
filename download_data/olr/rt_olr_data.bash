c308bf6d7b2ee1ef5cfefbf808d656c70ed6c7d8
hsushipei <hsushipei1@gmail.com>
2019-07-16 10:21:06 +0800

    1. Finish modify the constant year value to variable in the download part of "rt_olr_data.bash". 2. Delete old code of RT OLR download.

--- download_data/olr/rt_olr_data.bash
+++ download_data/olr/rt_olr_data.bash
@@ -5,12 +5,12 @@ yyyy=$(date +%Y) # current year
 
 echo "==== Enter 'rt_olr_data.bash'. Download and create realtime OLR data... ===="
 
-rm ${rtolrdir}olr-daily_v01r02-preliminary_20190101_latest.nc2
+rm ${rtolrdir}olr-daily_v01r02-preliminary_${yyyy}0101_latest.nc2
+
+wget http://olr.umd.edu/CDR/Daily/v01r02-interim/olr-daily_v01r02-preliminary_${yyyy}0101_latest.nc \
+	-O ${rtolrdir}olr-daily_v01r02-preliminary_${yyyy}0101_latest.nc
+
 exit
-
-wget http://olr.umd.edu/CDR/Daily/v01r02-interim/olr-daily_v01r02-preliminary_20190101_latest.nc \
-	-O ${rtolrdir}olr-daily_v01r02-preliminary_20190101_latest.nc
-
 # Create realtime OLR data (relative time)
 cdo -O -cat \
 	${rtolrdir}olr-daily_v01r02_20150101_prelim20181231.nc ${rtolrdir}olr-daily_v01r02-preliminary_20190101_latest.nc \
